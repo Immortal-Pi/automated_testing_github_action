ee54980ad2f44bcbb75b1ff7f0b622aeb59ecd22
amruth pai <26.amruth@gmail.com>
2025-03-10 16:13:22 -0500

    'test'

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -3,7 +3,8 @@ from src.math_operations import add, sub
 def test_add():
     assert add(2,3)==5 
     assert add(-1,1)==0
-
+    assert add(3,10)==13
+    assert add(-1,-1)==-2
 
 def test_sub():
     assert sub(-1,1)==-2
